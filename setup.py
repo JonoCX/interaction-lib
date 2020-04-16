242821f6ebb681b9db0d18667edb278049617d6f
Jonathan <jonathan.carlton@hotmail.com>
2020-04-16 08:13:25 +0100

    new setup

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
 from setuptools import setup, find_packages
 
-setup(name = 'interlib', packages = find_packages())
+name = 'interlib'
+version = 0.1
+description = 'A library for processing interaction data'
+url = 'https://github.com/JonoCX/interaction-lib'
+author = 'Jonathan Carlton'
+author_email = 'jonathan.carlton@bbc.co.uk'
+long_description = description
+
+setup(
+    name = name,
+    version = version,
+    description = description,
+    long_description = description,
+    url = url,
+    author = author,
+    author_email = author_email, 
+    packages = find_packages(where='interlib'),
+    install_requires=[
+        'numpy', 'scipy', 'pandas', 'joblib'
+    ]
+)
