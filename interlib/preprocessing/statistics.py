7a56eedefe800c0008e05e658e50db27d58d7b8f
Jonathan <jonathan.carlton@hotmail.com>
2020-04-16 17:10:29 +0100

    added tests for the time statistics, passing locally

--- interlib/preprocessing/statistics.py
+++ interlib/preprocessing/statistics.py
@@ -1,8 +1,6 @@
 """ """
 
 from .base import BaseExtractor
-from abc import ABCMeta
-from collections.abc import Mapping
 from joblib import Parallel, delayed, cpu_count
 
 import numpy as np 
@@ -20,14 +18,85 @@ class Statistics(BaseExtractor):
             raise ValueError('User event dictionary must have at least one value or not None')
         
         super(Statistics, self).__init__(
-            user_event_dict = user_event_dict,
+            user_event_dict = user_event_dict.copy(),
             n_jobs = n_jobs
         )
 
         self._statistics = {user: {} for user, d in self.data.items()}
         self._time_statistics = {}
 
-    def calculate_session_length(self, user_id = None):
+    def calculate_time_statistics(self, verbose = 0):
+        """ 
+            Using the user event data, calculate the hidden time, raw
+            session length, and session length of the users.
+
+            :params verbose: passed to the joblib backend
+            :returns: dictionary of results {user -> {hidden_time: 0...}}
+        """
+        def _get_stats(user_chunk, data_chunk):
+            user_dict = {user: [] for user in user_chunk}
+            for d in data_chunk: user_dict[d['user']].append(d)
+
+            results = {user: {} for user in user_chunk}
+
+            timestamps = {}
+            no_events_set = set([])
+            # calculate the hidden time (and get the timestamps)
+            for user, events in user_dict.items():
+                if len(events) < 1: # if there are no events
+                    no_events_set.add(user) # essentially set a flag per user
+                    results[user].update({'hidden_time': 0.0, 'raw_session_length': 0.0}) # set hidden time/raw to 0.0
+                    continue # move onto the next user
+
+                timestamps[user] = [event['timestamp'] for event in events] # collect timestamps
+
+                hidden_times = []
+                for index, event in enumerate(events): # for all events, if there is a visibility change to hidden
+                    if event['action_name'] == 'BROWSER_VISIBILITY_CHANGE' and event['data']['romper_to_state'] == 'hidden':
+                        hidden_ts = event['timestamp'] # record the timestamp
+
+                        if (index + 1) == len(events): break # if it's at the end, exit
+
+                        visible_ts = None
+                        for f_index, f_event in enumerate(events[index:]): # otherwise, loop forward in the event to find next BVC
+                            if f_event['action_name'] == 'BROWSER_VISIBILITY_CHANGE' and f_event['data']['romper_to_state'] == 'visible':
+                                visible_ts = f_event['timestamp']
+                                break
+
+                        if visible_ts: # if found, calculate the difference
+                            hidden_times.append((visible_ts - hidden_ts).total_seconds())
+                
+                # record the sum of the hidden times.
+                results[user].update({'hidden_time': np.sum(hidden_times)})
+
+            # calculate the raw session length
+            for user, ts in timestamps.items():
+                results[user].update({'raw_session_length': (ts[-1] - ts[0]).total_seconds()})
+
+            for user, res in results.copy().items(): # update the results with the session length
+                if user in no_events_set: sess_length = 0.0
+                else: sess_length = res['raw_session_length'] - res['hidden_time']
+                results[user].update({'session_length': sess_length})
+
+            return results
+
+        if not self._time_statistics: # we've not already calculated or we're being forced to do so
+            self._time_statistics = {user: {} for user, d in self.data.items()}
+            parallel = Parallel(n_jobs = self._num_cpu, verbose = verbose)
+
+            # run the process to calculate the statistics
+            res = parallel(delayed(_get_stats) (u, e) for u, e in self._users_split)
+            
+            # unpack the results into the time statistics dictionary
+            for r in res:
+                for u, s in r.items():
+                    self._time_statistics[u].update(s)
+                
+            return self._time_statistics
+        else: # otherwise just return the pre-calculate statistics
+            return self._time_statistics
+
+    def calculate_session_length(self, user_id = None, verbose = 0):
         """ 
             Calculate the session length only.
 
@@ -36,51 +105,19 @@ class Statistics(BaseExtractor):
             :returns: the session length or 
             all session lengths as dictionary {user -> session length}
         """
-        return None
+        if self._time_statistics:
+            if user_id:
+                if user_id not in self._time_statistics.keys():
+                    raise ValueError('Invalid user id (perhaps the user is not in the data)')
 
-    def calculate_session_length_statistics(self):
-        """ 
-            Given the dictionary of user events, calculate the session
-            length statistics for each users.
-
-            :returns: dictionary with user ids mapped to a inner dictionary
-            of session length, session_length_minutes, raw_session_length,
-            and hidden_time
-        """
-        def _get_timestamps(user_chunk, data_chunk):
-            user_dict = {user: [] for user in user_chunk}
-            for d in data_chunk: user_dict[d['user']].append(d)
-
-            timestamps = {}
-            for user, data in user_dict.items():
-                timestamps[user] = [event['timestamp'] for event in data]
-            return timestamps
-
-        def _get_session_lengths(timestamps):
-            return (timestamps[-1] - timestamps[0]).total_seconds()
-            # session_lengths = {}
-            # for u, ts in timestamps.items():
-            #     session_lengths[u] = (ts[-1] - ts[0]).total_seconds()
-            # return session_lengths
-
-        def _calculate_hidden_time(user_chunk, data_chunk):
-            user_dict = {user: [] for user in user_chunk}
-            for d in data_chunk: user_dict[d['user']].append(d)
-
-            return None
-
-        if not self._time_statistics:
-            self._time_statistics = {user: {} for user, d in self.data.items()}
-            parallel = Parallel(n_jobs = self._num_cpu)
-
-            timestamps = {}
-            for ts in parallel(delayed(_get_timestamps) (u, e) for u, e in self._users_split):
-                timestamps.update(ts)
-
-            # calculate the raw session length from the timestamps
-            for u, ts in timestamps.items():
-                self._time_statistics[u].update({'raw_session_length': _get_session_lengths(ts)})
-
-            return self._time_statistics
+                return self._time_statistics[user_id]['session_length']
+            return {user: stat['session_length'] for user, stat in self._time_statistics.items()}
         else:
-            return self._time_statistics
+            self.calculate_time_statistics(verbose = verbose)
+
+            if user_id:
+                if user_id not in self._time_statistics.keys():
+                    raise ValueError('Invalid user id (perhaps the user is not in the data)')
+                return self._time_statistics[user_id]['session_length']
+            
+            return {user: stat['session_length'] for user, stat in self._time_statistics.items()}
--- tests/test_preprocessing/test_stats.py
+++ tests/test_preprocessing/test_stats.py
@@ -17,7 +17,7 @@ def ground_truth():
         data = json.load(data_in)
     return data
 
-# Tests
+# ------ INIT ------
 def test_init(test_data):
     with pytest.raises(ValueError):
         stats = Statistics(user_event_dict = {})
@@ -29,21 +29,79 @@ def test_init(test_data):
     assert stats.data == test_data
     assert stats.n_jobs == -1
     
-def test_split_users(test_data):
+# ----- SPLIT USERS ------
+def test_split_users_correct_chunks(test_data):
+    # test that the data is split up into the correct chunks
     stats = Statistics(test_data, n_jobs = 4)
     for i, (u_chunk, d_chunk) in enumerate(stats._users_split):
         if i == 0: assert len(u_chunk) == 4
         else: assert len(u_chunk) == 3
 
-def test_calculate_session_length_statistics(test_data, ground_truth):
+def test_split_users_single_array(test_data):
+    # test that when the n jobs is 1 that the array remains a single array
+    stats = Statistics(test_data, n_jobs = 1)
+    for u_chunk, d_chunk in stats._users_split:
+        assert len(u_chunk) == len(test_data)
+
+def test_split_users_correspond_to_data_chunk(test_data):
+    # test that all of the events in the data chunk correspond to that user
     stats = Statistics(test_data, n_jobs = -1)
-    res = stats.calculate_session_length_statistics()
+    for u_chunk, d_chunk in stats._users_split: 
+        # get the set of users in the d chunk and compare with the u_chunk
+        users_in_d_chunk = set([e['user'] for e in d_chunk])
+        assert len(users_in_d_chunk) == len(u_chunk)
+        assert set(u_chunk.tolist()) == users_in_d_chunk
 
-    print(res)
-    # test the raw session length
+# ----- TIME STATISTICS -----
+def test_time_statistics(test_data, ground_truth):
+    stats = Statistics(test_data, n_jobs = -1)
+    res = stats.calculate_time_statistics()
+    
+    # test the the statistics calculated (with some allowance in the precision)
     for u, s in ground_truth.items():
-        print(res[u]['raw_session_length'])
         raw_sess_len = res[u]['raw_session_length']
-        
-        assert s['raw_session_length'] == pytest.approx(raw_sess_len, 0.1)
+        hidden_time = res[u]['hidden_time']
+        sess_len = res[u]['session_length']
 
+        assert s['raw_session_length'] == pytest.approx(raw_sess_len, 0.1)
+        assert s['hidden_time'] == pytest.approx(hidden_time, 0.1)
+        assert s['session_length'] == pytest.approx(sess_len, 0.1)
+
+def test_time_statistics_empty_events(test_data, ground_truth):
+    """ Test that empty events are dealt with """
+    test_data_copy = test_data.copy()
+    user_to_delete = list(test_data_copy.keys())[0]
+    test_data_copy[user_to_delete] = [] # remove their events
+
+    stats = Statistics(test_data_copy)
+    res = stats.calculate_time_statistics()
+
+    assert res[user_to_delete]['raw_session_length'] == 0.0
+    assert res[user_to_delete]['hidden_time'] == 0.0
+    assert res[user_to_delete]['session_length'] == 0.0
+
+def test_that_statistics_are_not_recalculated(test_data):
+    stats = Statistics(test_data)
+    res_one = stats.calculate_time_statistics()
+    res_two = stats.calculate_time_statistics()
+
+    assert res_one == res_two
+
+def test_getting_only_session_length(test_data, ground_truth):
+    stats = Statistics(test_data)
+    res = stats.calculate_session_length()
+    
+    for user, stat in res.items():
+        assert ground_truth[user]['session_length'] == pytest.approx(stat, 0.1)
+
+def test_getting_only_session_length_one_user(test_data, ground_truth):
+    stats = Statistics(test_data)
+    
+    user_to_retrieve = list(test_data.keys())[0]
+    sess_len = stats.calculate_session_length(user_id = user_to_retrieve)
+
+    assert ground_truth[user_to_retrieve]['session_length'] == pytest.approx(sess_len, 0.1)
+
+    # test that value error is raised if the user doesn't exist
+    with pytest.raises(ValueError):
+        stats.calculate_session_length(user_id = 'user_id')
