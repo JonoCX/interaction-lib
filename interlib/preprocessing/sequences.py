d8dfece7879e39d0c63df6c2c6ac00bffef0a1b2
Jonathan <jonathan.carlton@hotmail.com>
2020-05-15 14:59:11 +0100

    added compression functionality

--- interlib/preprocessing/sequences.py
+++ interlib/preprocessing/sequences.py
@@ -13,6 +13,36 @@ class Sequences(BaseExtractor):
     def __init__(self, user_event_dict, completion_point=None, n_jobs=-1):
         super().__init__(user_event_dict, completion_point=completion_point, n_jobs=n_jobs)
 
+    def _compress_events(self, sequence: List[str], compress_event: str = 'NEC') -> List[str]:
+        """ """
+        if len(sequence) == 0:
+            return sequence
+
+        updated_sequence = []
+        just_seen_event = False
+        for indx, event in enumerate(sequence): # for each event in the sequence
+
+            # if the event is the event to be compressed and we've not just seen it
+            if event == compress_event and not just_seen_event:
+                count = 1
+                for other_event in sequence[(indx + 1): ]: # scan forward to count
+                    if other_event == compress_event: count += 1
+                    else: break
+                
+                just_seen_event = True # indicate that we've just seen it
+                updated_sequence.append(event + '_' + str(count)) # event_count format
+            
+            # if we've not just seen the event, the append the next one
+            if not just_seen_event:
+                updated_sequence.append(event)
+            
+            # if we have just seen the event and the event isn't the compress event
+            if just_seen_event and event != compress_event:
+                updated_sequence.append(event)
+                just_seen_event = False # set just seen to false
+        
+        return updated_sequence
+
     def get_sequences(
         self,
         interaction_events: List[str],
--- tests/test_preprocessing/test_sequences.py
+++ tests/test_preprocessing/test_sequences.py
@@ -9,3 +9,34 @@ def test_data():
     with open('tests/test_data_files/test_data.p', 'rb') as data_in:
         data = pickle.load(data_in)
     return data
+
+# ------ COMPRESS SEQUENCE TESTS ------
+def test_compress_sequence(test_data):
+    sequence = ['NEC', 'PP', 'NEC', 'NEC', 'NEC', 'BB', 'NB', 'NEC']
+    expected_sequence = ['NEC_1', 'PP', 'NEC_3', 'BB', 'NB', 'NEC_1']
+
+    seq = Sequences(test_data, n_jobs = 1)
+    compressed_sequence = seq._compress_events(sequence)
+
+    for indx, val in enumerate(compressed_sequence):
+        assert val == expected_sequence[indx]
+
+def test_compress_sequence_link_choice(test_data):
+    sequence = ['LC', 'LC', 'LC', 'WOC', 'LC', 'NEC', 'LC', 'NB', 'BB', 'PP']
+    expected_sequence = ['LC_3', 'WOC', 'LC_1', 'NEC', 'LC_1', 'NB', 'BB', 'PP']
+
+    seq = Sequences(test_data, n_jobs = 1)
+    compressed_sequence = seq._compress_events(sequence, compress_event = 'LC')
+
+    for indx, val in enumerate(compressed_sequence):
+        assert val == expected_sequence[indx]
+
+def test_compress_sequence_no_compression_needed(test_data):
+    sequence = ['LC', 'NEC', 'PP', 'NB', 'BB', 'SP', 'LP', 'MP', 'VLP']
+    expected_sequence = sequence
+
+    seq = Sequences(test_data, n_jobs = 1)
+    compressed_sequence = seq._compress_events(sequence, compress_event = '')
+
+    for indx, val in enumerate(compressed_sequence):
+        assert val == expected_sequence[indx]
