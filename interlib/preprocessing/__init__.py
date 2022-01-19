844827744d6c0c95ccf6a2d4c98c415902591707
Jonathan <jonathan.carlton@hotmail.com>
2022-01-19 15:11:14 +0000

    updating library ready for release

--- interlib/preprocessing/__init__.py
+++
@@ -1,3 +0,0 @@
-from .sequences import *
-from .statistics import *
-from .slices import *
--- interlib/util/__init__.py
+++
@@ -1,8 +0,0 @@
-""" 
-Utility functions and helpers for the library
-"""
-
-# ----- Imports ------
-
-from .helpers import *
-from .data import *
